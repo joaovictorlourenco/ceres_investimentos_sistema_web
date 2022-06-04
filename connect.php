33bbbcb2ff1b518bb063ce465255d688a59ebf3c
João Victor Lourenço Barbosa Bernardo Diniz <87540912+joaovictorlourenco@users.noreply.github.com>
2022-06-04 16:14:53 -0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -7,16 +7,6 @@ $pass = "";
 $db_name = "user_ceres";
 
 
-
-/* Deploy */
-/*
-$server = "sql300.epizy.com";
-$user = "id19017654_user";
-$pass = "dsRsl1bUoFh61";
-
-$db_name = "epiz_31848274_user";
-*/
-
 $conn = mysqli_connect($server, $user, $pass, $db_name) or ('Não foi possível conectar com o banco de dados');
 
 ?>
